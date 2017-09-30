5d2521c9465443603613d15cd8cd7dd41dca5515
JeromeOrtwein <JeromeOrtwein@web.de>
2017-09-30 18:58:14 +0200

    up

--- src/com/stupro/uhc/Main.java
+++ src/com/stupro/uhc/Main.java
@@ -1,12 +1,11 @@
 package com.stupro.uhc;
 
-
 import javafx.application.Application;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 	public static void main(String args[]) throws Exception {
-//		//Database call 
+		//Database call 
 //				DatabaseIntegration test = new DatabaseIntegration();
 //				ResultSet rs;
 //				rs = test.displaySHO();
